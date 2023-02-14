2c98c137ebad755495e06489f0dc1f73ff32997b
Volodya Ismailyan <nenie_iri@mail.ru>
2023-02-14 19:12:46 +0400

    [ADD] main window sizes and geometry

--- COM_port_reader/Srcs/mainwindow.cpp
+++ COM_port_reader/Srcs/mainwindow.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 #include "../ui_mainwindow.h"
 
+#include <QApplication>
+#include <QScreen>
+    
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    
+    QScreen *screen = QApplication::primaryScreen();
+    QSize screenSize = screen->size();
+    int screenWidth = screenSize.width();
+    int screenHeight = screenSize.height();
+    int windownWidth = 400;
+    int windownHeight = 400;
+    this->setGeometry((screenWidth - windownWidth) / 2, \
+                      (screenHeight - windownHeight) / 2, \
+                      windownWidth, windownHeight);
+    
     this->setWindowTitle("OQNI: COM port reader");
     
     this->setWindowIcon(QIcon(":/Imgs/oqni.ico"));
