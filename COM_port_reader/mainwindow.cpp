9a5425128404f12d597071ce0193904dd84ec940
Volodya Ismailyan <nenie_iri@mail.ru>
2023-02-14 15:54:25 +0400

    [UPDATE] main window title

--- COM_port_reader/mainwindow.cpp
+++ COM_port_reader/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("OQNI: COM port reader");
+    this->setWindowIcon();
 }
 
 MainWindow::~MainWindow()
--- COM_port_reader/mainwindow.h
+++ COM_port_reader/mainwindow.h
@@ -11,11 +11,11 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
     
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-    
-private:
-    Ui::MainWindow *ui;
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
+        
+    private:
+        Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
