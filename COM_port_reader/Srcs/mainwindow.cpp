1c42a510b71a0ff2b3e92f48299dd9d9cbf7a660
Volodya Ismailyan <nenie_iri@mail.ru>
2023-02-14 19:59:00 +0400

    [ADD] background

--- COM_port_reader/Srcs/mainwindow.cpp
+++ COM_port_reader/Srcs/mainwindow.cpp
@@ -10,20 +10,27 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     
+    /* ------ put window to the center of the screen ------ */
+    
     QScreen *screen = QApplication::primaryScreen();
     QSize screenSize = screen->size();
     int screenWidth = screenSize.width();
     int screenHeight = screenSize.height();
-    int windownWidth = 400;
-    int windownHeight = 400;
+    int windownWidth = 500;
+    int windownHeight = 440;
     this->setGeometry((screenWidth - windownWidth) / 2, \
                       (screenHeight - windownHeight) / 2, \
                       windownWidth, windownHeight);
+    
+    /* ---------------------------------------------------- */
     
     this->setWindowTitle("OQNI: COM port reader");
     
     this->setWindowIcon(QIcon(":/Imgs/oqni.ico"));
     this->setWindowFilePath(":/Imgs/oqni.ico");
+    
+    QString background = ":/Imgs/background.png";
+    this->setStyleSheet("background-color: #CCD6DD; background-image: url(" + background + "); background-repeat: no-repeat; background-position: center;");
 }
 
 MainWindow::~MainWindow()
